cd1e14bd08c0dfe6aec983f6e5618342da53a199
alexomarchali <122356273+alexomarchali@users.noreply.github.com>
2023-10-23 20:08:57 +0200

    overezichtExamen

--- app/views/Instructeur/overzichtInstructeur.php
+++ app/views/Instructeur/overzichtInstructeur.php
@@ -2,12 +2,12 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,1,200" />
-    <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
-    <title>Overzicht Instructeurs</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,1,200" />
+<link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
+<title>Overzicht Instructeurs</title>
 </head>
 
 <body>
@@ -25,8 +25,8 @@
                 <th>Aantal sterren</th>
                 <th>Voertuigen</th>
             </thead>
-            <tbody>
-                <?= $data['rows']; ?>
+                <tbody>
+                    <?= $data['rows']; ?>
             </tbody>
         </table>
     </div>
