ac434efd5d2d0884219f352010ca4693e31e1bb8
Pershiya Sharifi <335880@mboutrecht.nl>
2023-11-02 16:25:20 +0100

    ninth commit

--- app/views/Voertuigen/editVoertuig.php
+++ app/views/Voertuigen/editVoertuig.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,1,200" />
-<link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
-<title>Overzicht voertuigen</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,1,200" />
+    <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
+    <title>Overzicht voertuigen</title>
 </head>
+
 <body>
     <h1><?= $data['title']; ?></h1>
     <form method="post">
     </form>
     <a href="<?= URLROOT . "/Homepage" ?>" class="button">Back</a>
 </body>
-</html>
 
+</html>
